547555940b9aaa305dffb2d5feec5288f14f6a84
Alexander-Lee Couchy <alexander.couchy@gmail.com>
2020-11-02 18:28:11 +0100

    Change to mongoose for db connection

--- db.js
+++ db.js
@@ -1,5 +1,9 @@
 require("dotenv").config()
 const MongoClient = require("mongodb").MongoClient
+const mongoose = require("mongoose")
+
+mongoose.connection.once('open', () => { console.log('MongoDB Connected') })
+mongoose.connection.on('error', (err) => { console.log('MongoDB connection error: ', err) })
 
 const state = {
     client: null,
@@ -7,6 +11,8 @@ const state = {
 
 exports.get = () => {
     if (state.client) return state.client
-    state.client = new MongoClient(process.env.DB_URL + "?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
+    //state.client = new MongoClient(process.env.DB_URL + "?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
+    const conn = mongoose.createConnection(process.env.DB_URL + "?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
+    state.client = conn.getClient()
     return state.client
 }
